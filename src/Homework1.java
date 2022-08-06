7b4f5147097add97a0f8dbb0ced5761cdae36017
SergeiPospelov <pospelka413@gmail.com>
2022-08-06 19:23:47 +0300

    added edge case

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,11 +1,16 @@
 public class Homework1 {
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 0, 2, 3, 0, 4, 5, 0, 6, 0, 6, 6};
+        int[] arr = new int[]{8,4,5,0,0,0,0,7};
         int duplicatesCount = 0;
         int indexCount = arr.length - 1;
         //Посчитаем количество нулей которые необходимо продублировать
         for (int i = 0; i <= indexCount - duplicatesCount; i++) {
             if (arr[i] == 0) {
+                if (i == indexCount - duplicatesCount) {
+                    arr[indexCount] = 0;
+                    indexCount -= 1;
+                    break;
+                }
                 duplicatesCount++;
             }
         }
