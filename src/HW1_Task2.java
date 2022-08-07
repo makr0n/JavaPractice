de23e90bcf852b577f2563baee87700bee148ccd
SergeiPospelov <pospelka413@gmail.com>
2022-08-07 16:34:05 +0300

    Added Task2 remove element

--- src/HW1_Task2.java
+++ src/HW1_Task2.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class HW1_Task2 {
+public class HW1_Task2 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,1,2,2,3,0,4,2};
+        int val = 3;
+        int i = 0;
+        int k = nums.length;
+        while (i < k) {
+            if (nums[i] == val) {
+                nums[i] = nums[k-1];
+                k--;
+            }else{
+                i++;
+            }
+
+        }
+        System.out.println(k);
+        Homework1.printArray(nums);
+
+    }
 }
